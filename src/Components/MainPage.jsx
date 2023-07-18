2815514eca0fd4b245bdbd34d68da93a5f45238c
slovetechnology <slovetech619@gmail.com>
2023-07-18 12:12:17 +0100

    fixings

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -16,6 +16,7 @@ export default function MainPage() {
         header: true,
         download: true,
         complete: (result) => {
+          console.log(result.data)
           return setPokes(result.data);
         }
       }
